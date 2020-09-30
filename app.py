565b11f0c7c846479412030c7b9a0be1ee633785
Paulo Salvatore <paulo@salvatore.tech>
2020-09-30 16:43:44 -0300

    Flask implementado e arquivo inicial criado

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import (
+    Blueprint, render_template, request
+)
+from flask import Flask
+
+app = Flask(__name__)
+
+bp = Blueprint('app', __name__)
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    return render_template('index.html')
+
+
+app.register_blueprint(bp)
